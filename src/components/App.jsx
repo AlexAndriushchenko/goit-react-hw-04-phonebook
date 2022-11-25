730ef0887c2ae9249841a4370bb680366c45cfaf
AlexAndriushchenko <aleksandr.work3@gmail.com>
2022-11-25 16:16:25 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import ContactForm from "./ContactForm/ContactForm ";
-import ContactList from "./ContactList/ContactList";
-import Filter from "./Filter/Filter";
-
+import ContactForm from './ContactForm/ContactForm ';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
 export default function App() {
-
   const [contacts, setContacts] = useState(() => {
     return JSON.parse(localStorage.getItem('contacts')) ?? [];
   });
@@ -14,8 +12,7 @@ export default function App() {
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
-  },[contacts]);
-
+  }, [contacts]);
 
   const addContact = data => {
     const newContact = {
@@ -24,19 +21,25 @@ export default function App() {
       number: data.number,
     };
 
-    for (const contact of contacts)
-    {
+    for (const contact of contacts) {
       if (contact.name === data.name) {
         window.alert(`${contact.name} is already in contacts.`);
         return;
-      }   
+      }
     }
+
+    // this.setState(prevState => ({
+    //   contacts: [newContact, ...prevState.contacts],
+    // }));
+
     setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
   const deleteContact = contactId => {
     // setContacts(state => state.contacts.filter(contact => contact.id !== contactId));
-    setContacts(prevContacts => prevContacts.filter(contact => contact.id !== contactId));
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
   const changeFilter = evt => {
@@ -45,23 +48,22 @@ export default function App() {
 
   const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
-  
 
   return (
-    <>   
-        <h1>Phonebook</h1>
-        <ContactForm onSubmitData={addContact} />
-      
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={changeFilter}/> 
-      <ContactList contacts={getVisibleContacts()} onDeleteContact={deleteContact} />       
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmitData={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={getVisibleContacts()}
+        onDeleteContact={deleteContact}
+      />
     </>
   );
-
-};
-
-
-
-
+}
